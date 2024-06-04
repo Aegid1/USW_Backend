58f5c596c0d7afabb09f629756ecb120675261f2
Ägidius Haslauer <aegidius.haslauer@ipk.fraunhofer.de>
2024-06-04 09:13:56 +0200

    implemented communication with open_ai assistant

--- app/api/chat_api.py
+++ app/api/chat_api.py
@@ -1,19 +1,44 @@
 from fastapi import APIRouter, Depends
 from ..services.chat_service import chat_service
-from ..services.open_ai_service import open_ai_service
+from ..services.open_ai_service import OpenAIService
+import time
 
 router = APIRouter()
+
 
 def get_chat_service():
     return chat_service()
 
+
 def get_open_ai_service():
-    return open_ai_service
+    return OpenAIService
+
 
 @router.post("/chat")
-async def create_thread_id(open_ai_service: open_ai_service =Depends()):
+async def create_thread_id(open_ai_service: OpenAIService = Depends()):
     thread_id = open_ai_service.create_thread_id()
     return thread_id
 
+
 @router.post("/chat/{thread_id}")
-async def chat(thread_id, message: dict):
+async def chat(thread_id, message: dict, open_ai_service: OpenAIService = Depends()):
+    message_text = message.get("text", "")
+    #store message in db
+
+    #send message to thread
+    message = open_ai_service.send_message_to_thread(thread_id, message_text)
+
+    #run thread with the assigned message
+    run = open_ai_service.execute_thread(thread_id)
+
+    # while run.status not in ['completed', 'failed']:
+    #     print(run.status)
+    #     time.sleep(1)
+    #     run = open_ai_service.retrieve_execution(thread_id, run.id)
+    #     if run.status == 'requires_action':
+    #         run = submit_tool_outputs(thread_id, run.id, run.required_action.submit_tool_outputs.tool_calls)
+
+    #retrieve messages from a thread
+    messages = open_ai_service.retrieve_messages_from_thread(thread_id)
+
+    return messages.data[0].content[0].text.value
--- app/services/open_ai_service.py
+++ app/services/open_ai_service.py
@@ -1,9 +1,41 @@
-class open_ai_service:
+import yaml
+from openai import OpenAI
+
+
+class OpenAIService:
+    config = yaml.safe_load(open("../config_empty.yaml"))
+    client = OpenAI(api_key=config['KEYS']['openai'])
+
+    assistant = client.beta.assistants.create(
+        name="Email Assistant",
+        instructions="You are an assistant who has access to Emails and the web.",
+        tools=[],
+        model="gpt-3.5-turbo-0125"
+    )
 
     def create_thread_id(self):
-        #TODO
-        pass
+        return self.client.beta.threads.create()
 
     def get_thread_id(self):
         #TODO
         pass
+
+    def send_message_to_thread(self, thread_id, message_text):
+        return self.client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=message_text
+        )
+
+    def execute_thread(self, thread_id):
+        return self.client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=self.assistant.id,
+            instructions="Please address the user as my Lord. The user has a premium account."
+        )
+
+    def retrieve_execution(self, thread_id, run_id):
+        return self.client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+
+    def retrieve_messages_from_thread(self, thread_id):
+        return self.client.beta.threads.messages.list(thread_id=thread_id)
