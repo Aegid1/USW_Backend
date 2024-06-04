37ebedf234f7f2be10cfb54131a58d29893c907f
Ägidius Haslauer <aegidius.haslauer@ipk.fraunhofer.de>
2024-06-04 09:32:36 +0200

    fix router paths in main.py

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from api.media_api import router as media_router
 
 app = FastAPI()
 
-app.include_router(chat_router, prefix="/api/db")
-app.include_router(media_router, prefix="/api/user")
+app.include_router(chat_router, prefix="/api/v1")
+app.include_router(media_router, prefix="/api/v1")
 
 app = FastAPI()
 
